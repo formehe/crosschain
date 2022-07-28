74f7ba9f2124b287e1eda77ca2582773f1cb89ac
forme628 <forme.he@topnetwork.org>
2022-07-28 14:13:49 +0800

    modify top mint privilege

--- contracts/contracts/Top/verify/VerifierUpgradeable.sol
+++ contracts/contracts/Top/verify/VerifierUpgradeable.sol
@@ -95,12 +95,12 @@ abstract contract VerifierUpgradeable is Initializable, AdminControlledUpgradeab
     }
 
     modifier mint_pauseable(){
-        require(!hasRole(BLACK_MINT_ROLE, _msgSender()) && ((paused & PAUSED_MINT) == 0),"no permit");
+        require(!hasRole(BLACK_MINT_ROLE, _msgSender())&& ((paused & PAUSED_MINT) == 0 || hasRole(CONTROLLED_ROLE,_msgSender())),"no permit");
         _;
     }
 
     modifier burn_pauseable(){
-        require(!hasRole(BLACK_BURN_ROLE, _msgSender())&& ((paused & PAUSED_BURN) == 0 || hasRole(CONTROLLED_ROLE,_msgSender())),"no permit");
+        require(!hasRole(BLACK_BURN_ROLE, _msgSender()) && ((paused & PAUSED_BURN) == 0),"no permit");
         _;
     }
 }
