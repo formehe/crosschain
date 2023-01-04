34d32f528d3ec218e79a245ef1f7ad37e6d34ae8
forme628 <forme.he@topnetwork.org>
2023-01-04 13:34:13 +0800

    add unit test for dao

--- contracts/contracts/Dao/TimeController.sol
+++ contracts/contracts/Dao/TimeController.sol
@@ -5,7 +5,13 @@ import "@openzeppelin/contracts/governance/TimelockController.sol";
 
 contract TimeController is TimelockController {
     constructor (uint256 minDelay, address[] memory proposers, address[] memory executors) TimelockController(minDelay, proposers, executors){
+        for (uint256 i = 0; i < proposers.length; ++i) {
+            require(proposers[i] != address(0), "proposer can not be zero");
+        }
 
+        for (uint256 i = 0; i < executors.length; ++i) {
+            require(executors[i] != address(0), "executor can not be zero");
+        }
     }
 
     function renounceRole(bytes32 /*role*/, address /*account*/) public pure override {
--- tests/testDao.js
+++ tests/testDao.js
@@ -4,6 +4,7 @@ const Web3 = require('web3');
 
 var utils = require('ethers').utils;
 const { AddressZero } = require("ethers").constants
+const { BigNumber } = require('ethers')
 
 const BN = require('bn.js');
 chai.use(require('chai-bn')(BN));
@@ -33,7 +34,7 @@ describe("TDao", function () {
         console.log("redeemaccount account:", redeemaccount.address)
 
         //deploy time lock controller
-        timelockcontrollerCon = await ethers.getContractFactory("TimelockController", deployer)
+        timelockcontrollerCon = await ethers.getContractFactory("TimeController", deployer)
         timelockcontroller = await timelockcontrollerCon.deploy(1,[],[])
         await timelockcontroller.deployed()
         console.log("+++++++++++++timelockcontroller+++++++++++++++ ", timelockcontroller.address)
@@ -53,6 +54,8 @@ describe("TDao", function () {
 
         await timelockcontroller.connect(deployer).grantRole("0xb09aa5aeb3702cfd50b6b62bc4532604938f21248a27a1d5ca736082b6819cc1", tdao.address)
         await timelockcontroller.connect(deployer).grantRole("0xd8aa0f3194971a2a116679f7c2090f6939c8d4e01a2a8d7e41d55e5351469e63", tdao.address)
+        await timelockcontroller.connect(deployer).grantRole("0xb09aa5aeb3702cfd50b6b62bc4532604938f21248a27a1d5ca736082b6819cc1", deployer.address)
+        await timelockcontroller.connect(deployer).grantRole("0xd8aa0f3194971a2a116679f7c2090f6939c8d4e01a2a8d7e41d55e5351469e63", deployer.address)
 
         erc20SampleCon = await ethers.getContractFactory("ERC20TokenSample", user)
         erc20Sample = await erc20SampleCon.deploy()
@@ -106,7 +109,6 @@ describe("TDao", function () {
             await expect(tdao.connect(user)["execute(uint256)"](tx.toBigInt())).to.be.revertedWith("TimelockController: underlying transaction reverted")
 
             transferCalldata = erc20Sample.interface.encodeFunctionData('transfer', [user.address, 128])
-            // tx = await tdao.connect(admin).callStatic["propose(address[],uint256[],bytes[],string)"]([erc20Sample.address], [0], [transferCalldata], "Proposal #1: Give grant to team")
             await expect(tdao.connect(admin)["propose(address[],uint256[],bytes[],string)"]([erc20Sample.address], [0], [transferCalldata], "Proposal #1: Give grant to team")).
             to.be.revertedWith("GovernorCompatibilityBravo: proposer votes below proposal threshold")
         } catch (e) {
@@ -118,11 +120,43 @@ describe("TDao", function () {
         await expect(votesCon.deploy([user.address, user1.address])).to.be.revertedWith("number of voters must more than 3")
         await expect(votesCon.deploy([user.address, user1.address, user.address])).to.be.revertedWith("voter can not be repeated")
         await expect(votesCon.deploy([user.address, user1.address, AddressZero])).to.be.revertedWith("invalid voter")
-        await expect(votes.getPastTotalSupply(BigNumber.from(1).shl(129))).to.be.revertedWith("block not yet mined")
-        await expect(votes.getPastVotes(user.address, BigNumber.from(1).shl(129))).to.be.revertedWith("block not yet mined")
-        await expect(votes.delegates(user.address)).to.be.revertedWith("not support")
-        await expect(votes.delegate(user.address)).to.be.revertedWith("not support")
-        await expect(votes.delegateBySig(user.address)).to.be.revertedWith("not support")
+        await expect(votes.getPastTotalSupply(BigNumber.from(1).shl(129))).to.be.revertedWith("Transaction reverted without a reason string")
+        await expect(votes.getPastVotes(user.address, BigNumber.from(1).shl(129))).to.be.revertedWith("Transaction reverted without a reason string")
+        await expect(votes.delegates(user.address)).to.be.revertedWith("Transaction reverted without a reason string")
+        await expect(votes.delegate(user.address)).to.be.revertedWith("Transaction reverted without a reason string")
+        // await expect(votes.delegateBySig(user.address)).to.be.revertedWith("Transaction reverted without a reason string")
+    })
+
+    it('time controller', async () => {
+        await expect(timelockcontrollerCon.deploy(1,[AddressZero],[])).to.be.revertedWith("proposer can not be zero")
+        await expect(timelockcontrollerCon.deploy(1,[],[AddressZero])).to.be.revertedWith("executor can not be zero")
+        await expect(timelockcontroller.updateDelay(2)).to.be.revertedWith("TimelockController: caller must be timelock")
+        const transferCalldata = timelockcontroller.interface.encodeFunctionData('updateDelay', [1])
+        await expect(timelockcontroller.executeBatch([timelockcontroller.address], [0], [transferCalldata], "0x0000000000000000000000000000000000000000000000000000000000000000", "0x0000000000000000000000000000000000000000000000000000000000000002")).
+        to.be.revertedWith("TimelockController: operation is not ready")
+        await expect(timelockcontroller.connect(user).executeBatch([timelockcontroller.address], [0], [transferCalldata], "0x0000000000000000000000000000000000000000000000000000000000000000", "0x0000000000000000000000000000000000000000000000000000000000000002")).
+        to.be.revertedWith("is missing role")
+        await expect(timelockcontroller.executeBatch([], [0], [transferCalldata], "0x0000000000000000000000000000000000000000000000000000000000000000", "0x0000000000000000000000000000000000000000000000000000000000000002")).
+        to.be.revertedWith("TimelockController: length mismatch")
+        await expect(timelockcontroller.executeBatch([], [], [transferCalldata], "0x0000000000000000000000000000000000000000000000000000000000000000", "0x0000000000000000000000000000000000000000000000000000000000000002")).
+        to.be.revertedWith("TimelockController: length mismatch")
+
+        await expect(timelockcontroller.scheduleBatch([timelockcontroller.address], [0], [transferCalldata], "0x0000000000000000000000000000000000000000000000000000000000000000", "0x0000000000000000000000000000000000000000000000000000000000000002", 0)).
+        to.be.revertedWith("TimelockController: insufficient delay")
+
+        await timelockcontroller.scheduleBatch([timelockcontroller.address], [0], [transferCalldata], "0x0000000000000000000000000000000000000000000000000000000000000000", "0x0000000000000000000000000000000000000000000000000000000000000002", 2)
+        await expect(timelockcontroller.executeBatch([timelockcontroller.address], [0], [transferCalldata], "0x0000000000000000000000000000000000000000000000000000000000000000", "0x0000000000000000000000000000000000000000000000000000000000000002")).
+        to.be.revertedWith("TimelockController: operation is not ready")
+        await expect(timelockcontroller.scheduleBatch([timelockcontroller.address], [0], [transferCalldata], "0x0000000000000000000000000000000000000000000000000000000000000000", "0x0000000000000000000000000000000000000000000000000000000000000002", 1)).
+        to.be.revertedWith("TimelockController: operation already scheduled")
+        await new Promise(r => setTimeout(r, 1000))
+        await timelockcontroller.executeBatch([timelockcontroller.address], [0], [transferCalldata], "0x0000000000000000000000000000000000000000000000000000000000000000", "0x0000000000000000000000000000000000000000000000000000000000000002")
+        const tx = await timelockcontroller.hashOperationBatch([timelockcontroller.address], [0], [transferCalldata], "0x0000000000000000000000000000000000000000000000000000000000000000", "0x0000000000000000000000000000000000000000000000000000000000000002")
+        await expect(timelockcontroller.cancel(tx)).to.be.revertedWith("TimelockController: operation cannot be cancelled")
+        await timelockcontroller.scheduleBatch([timelockcontroller.address], [0], [transferCalldata], "0x0000000000000000000000000000000000000000000000000000000000000002", "0x0000000000000000000000000000000000000000000000000000000000000002", 1)
+        await new Promise(r => setTimeout(r, 1000))
+        await expect(timelockcontroller.executeBatch([timelockcontroller.address], [0], [transferCalldata], "0x0000000000000000000000000000000000000000000000000000000000000002", "0x0000000000000000000000000000000000000000000000000000000000000002")).
+        to.be.revertedWith("TimelockController: missing dependency")
     })
 
     it('Proposal', async () => {
