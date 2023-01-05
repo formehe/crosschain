9aa9c710734632d61fa410e99ad6517b800f4d2f
forme628 <forme.he@topnetwork.org>
2023-01-05 17:16:23 +0800

    modify for bug of adding block

--- contracts/contracts/Eth/bridge/TopBridge.sol
+++ contracts/contracts/Eth/bridge/TopBridge.sol
@@ -186,12 +186,18 @@ contract TopBridge is  ITopBridge, AdminControlledUpgradeable, IGovernanceCapabi
     }
  
     /// @dev Gets the validated election block
-    function getValidationEpoch(uint64 epochId) private view returns(Epoch memory epoch){
+	function getValidationEpoch(uint64 epochId) private view returns(Epoch memory epoch){
         uint cnt = epochs.length;
-        for (uint i = 0; i < cnt; i++) {
+        for ((uint i, uint num) = (currentEpochIdex, 0); num < cnt; num++) {
             if(epochs[i].epochId == epochId){
                 epoch = epochs[i];
                 break;
+            }
+
+            if (i == (cnt - 1)) {
+                i = 0;
+            } else {
+                i++;
             }
         }
 
